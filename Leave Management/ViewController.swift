fa57603ea7cb34930f5caf9a131bfa369ccbf085
domi <dommietabu@gmail.com>
2018-12-05 12:15:35 +0300

    Initial Commit

---
+++ Leave Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leave Management
+//
+//  Created by Dominic Tabu on 05/12/2018.
+//  Copyright © 2018 Ellixar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
